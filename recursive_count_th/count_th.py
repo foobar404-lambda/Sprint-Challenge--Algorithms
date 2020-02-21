75e18123bcc8993a7ce240c2894c570500f79497
foobar404 <austinthemichaud@gmail.com>
2020-02-21 11:05:54 -0800

    finish sorting robot & count_th

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -1,10 +1,19 @@
-'''
+"""
 Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
-'''
+"""
+
+
+def recursive_help(word, count=0):
+    if len(word) <= 1:
+        return count
+    if word[:2] == "th":
+        count += 1
+
+    return recursive_help(word[1:], count)
+
+
 def count_th(word):
-    
-    # TBC
-    
-    pass
+    return recursive_help(word, count=0)
+
